de44ef3880ebf76b68c793610904584ff3596a63
Pau Mesonero <paumesonero@Caitlins-MacBook-Air.local>
2022-07-27 13:21:14 +0200

    iterations 1-2 done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import contactsData from './contacts.json'
 
 function App() {
+  const [contacts, setContacts] = useState(contactsData)
+  const firstFive = contacts.slice(0, 5)
+  console.log(firstFive)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <table className='table-contacts'>
+        <thead className='t-head'>
+          <tr className='table-heading-row'>
+            <th className='table-heading' >Picture</th>
+            <th className='table-heading'>Name</th>
+            <th className='table-heading'>Popularity</th>
+            <th className='table-heading'>Won <br /> Oscar</th>
+            <th className='table-heading'>Won <br /> Emmy</th>
+          </tr>
+        </thead>
+        <tbody className='t-body'>
+          {firstFive.map(el => {
+            let roundedPopularity = el.popularity.toFixed(2)
+            let oscarWon = el.wonOscar && <p>🏆</p>;
+            let emmyWon = el.wonEmmy && <p>🏆</p>;
+
+            return (
+              <tr className='table-body-row'>
+                <td id='picture-contact' className='table-body-data'>
+                  <img src={el.pictureUrl} alt={el.name} width='100px' />
+                </td>
+                <td className='table-body-data'>
+                  <p className='name-popu'> {el.name}</p>
+                </td>
+                <td className='table-body-data'>
+                  <p className='name-popu'> {roundedPopularity}</p>
+                </td>
+                <td className='table-body-data'>
+                  {oscarWon}
+                </td>
+                <td className='table-body-data'>
+                  {emmyWon}
+                </td>
+
+              </tr>
+
+            )
+
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
